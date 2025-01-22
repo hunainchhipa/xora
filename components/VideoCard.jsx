41af0c5d0beadb5962bf7bd3849217f0a5aa4253
Hunain <hunain.chhipa23@gmail.com>
2025-01-22 11:14:15 +0530

    created video section in home screen.

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,5 +1,6 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { icons } from "../constants";
 
 const VideoCard = ({
   video: {
@@ -9,11 +10,13 @@ const VideoCard = ({
     creator: { username, avatar },
   },
 }) => {
+  const [play, setPlay] = useState(false);
+
   return (
     <View className="flex-col items-center x-4 mb-14">
       <View className="flex-row gap-3 items-start">
         <View className="justify-center items-center flex-row flex-1">
-          <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+          <View className="w-[46px] h-[46px] rounded-lg justify-center items-center p-0.5">
             <Image
               source={{ uri: avatar }}
               className="w-full h-full rounded-lg"
@@ -31,7 +34,32 @@ const VideoCard = ({
             </Text>
           </View>
         </View>
+
+        <View className="pt-2">
+          <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
+        </View>
       </View>
+
+      {play ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+          className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
+        >
+          <Image
+            source={{ uri: thumbnail }}
+            className="w-full h-full rounded-xl mt-3"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
